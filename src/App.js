8e10d30c97f51b9f417ed67f6a60ec9f941d0cff
Melissa <melmihomel@gmail.com>
2022-09-18 17:52:48 -0300

    versao 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar"
+import Corpo from "./Corpo"
+
+export default function App(){
+    console.log("oisss")
+    return (
+        <div>
+            <NavBar/>
+            <Corpo/>
+        </div>
+        
+    )
+}
