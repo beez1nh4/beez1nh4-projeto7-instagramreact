343d9378436599e51a847d31260519b4a08dc3a8
Melissa <melmihomel@gmail.com>
2022-09-19 16:36:32 -0300

    versao com null

--- src/Usuario.js
+++ src/Usuario.js
@@ -9,11 +9,11 @@ function Info(props){
     }
     return (
         <div class="usuario">
-            <img src={foto} onClick={() => setFoto(prompt("Novo link de foto de perfil:"))} alt=""/>
+            <img src={(foto=== ""|| foto === null) ? props.foto : foto} onClick={() => setFoto(prompt("Novo link de foto de perfil:"))} alt=""/>
             <div class="texto" >
               <strong >{props.usuario}</strong>
               <span>
-                {apelido}
+              {(apelido=== ""|| apelido === null) ? props.apelido : apelido}
              <ion-icon onClick={changeNames} name="pencil"></ion-icon>
              </span>
             </div>
